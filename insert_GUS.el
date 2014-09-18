d11bd19995e976eff7cee4ad970211473854dfca
Nolan Prescott <prescott.nolan@gmail.com>
2014-09-18 00:29:37 -0500

    simplify alphanumeric check

--- insert_GUS.el
+++ insert_GUS.el
@@ -2,6 +2,7 @@
 ;;; by Christopher Wellons. 2011-11-18. Editted by Xah Lee.
 ;;; Edited by Hideki Saito further to generate all valid variants for "N"
 ;;; in xxxxxxxx-xxxx-Mxxx-Nxxx-xxxxxxxxxxxx format.
+
 (defun np-insert-uuid-internal ()
   "Insert a UUID. This uses a simple hashing of variable data."
   (let ((myStr
@@ -18,23 +19,21 @@
 		      (recent-keys)))))
 
     (format "%s-%s-4%s-%s%s-%s"
-                    (substring myStr 0 8)
-                    (substring myStr 8 12)
-                    (substring myStr 13 16)
-		    (format "%x" (+ 8 (random 4)))
-                    (substring myStr 17 20)
-                    (substring myStr 20 32))))
+	    (substring myStr 0 8)
+	    (substring myStr 8 12)
+	    (substring myStr 13 16)
+	    (format "%x" (+ 8 (random 4)))
+	    (substring myStr 17 20)
+	    (substring myStr 20 32))))
 
-(defun np-contains-number (var-string)
-  (string-match-p "[0-9]" var-string))
+(defun np-is-valid-gus-id (var-string)
+  (and (string-match-p "[0-9]" var-string)
+       (string-match-p "[a-f]" var-string)))
 
-(defun np-contains-letter (var-string)
-  (string-match-p "[A-Fa-f]" var-string))
-
-(defun np-insert-GUS ()
+(defun np-insert-gus ()
   "Insert a 5 character alphanumeric hash (ex. 62b57) at point"
   (interactive)
   (let ((subStr (substring (np-insert-uuid-internal) 31 36)))
-    (if (and (np-contains-number subStr) (np-contains-letter subStr))
+    (if (np-is-valid-gus-id subStr)
 	(insert subStr)
-      (np-insert-GUS))))
+      (np-insert-gus))))
