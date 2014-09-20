2c72b41fcd1e078046d73d45bf2d987499d52877
Nolan Prescott <prescott.nolan@gmail.com>
2014-09-19 22:46:12 -0500

    I'm going to stop now

--- insert_gus.el
+++ insert_gus.el
@@ -1,7 +1,7 @@
 ; heavy inspiration taken from:
 ; http://ergoemacs.org/emacs/elisp_generate_uuid.html
 
-(defun insert-gus-selector ()
+(defun insert-gus-hash ()
   (interactive)
   "Insert a random 5 character hash (ex. 309ec) at point, for use with Globally
 Unique Selector (GUS) syntax. Taken from an md5 hash of variable system data."
@@ -17,4 +17,4 @@ Unique Selector (GUS) syntax. Taken from an md5 hash of variable system data."
     (if (and (string-match-p "[0-9]" myStr)
 	     (string-match-p "[a-f]" myStr))
 	(insert myStr)
-      (insert-gus-selector))))
+      (insert-gus-hash))))
