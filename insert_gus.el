048d2e698784e24b34b21cd6f2b5a06899411347
Nolan Prescott <prescott.nolan@gmail.com>
2014-09-19 23:19:04 -0500

    Oops, gotta keep the docstring in place

--- insert_gus.el
+++ insert_gus.el
@@ -2,9 +2,10 @@
 ; http://ergoemacs.org/emacs/elisp_generate_uuid.html
 
 (defun insert-gus-hash ()
+  "Insert a random 5 character alphanumeric hash (ex. 309ec) at point, for use 
+with Globally Unique Selector (GUS) syntax. Taken from an md5 hash of variable 
+system data."
   (interactive)
-  "Insert a random 5 character hash (ex. 309ec) at point, for use with Globally
-Unique Selector (GUS) syntax. Taken from an md5 hash of variable system data."
   (let ((myStr (substring
 		(md5 (format "%s%s%s%s%s%s"
 			     (emacs-pid)
