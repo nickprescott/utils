6df8a0daebcbc6f8a5ef1603ac7e6c725c28240c
Nolan Prescott <prescott.nolan@gmail.com>
2014-09-19 22:24:45 -0500

    no one will ever need a whole UUID-4...

---
+++ insert_gus.el
@@ -0,0 +1,19 @@
+; heavy inspiration taken from:
+; http://ergoemacs.org/emacs/elisp_generate_uuid.html
+
+(defun insert-gus-selector ()
+  "Insert a random 5 character hash (ex. 309ec) at point, for use with Globally
+Unique Selector (GUS) syntax. Taken from an md5 hash of variable system data."
+  (let ((myStr (substring
+		(md5 (format "%s%s%s%s%s%s"
+			     (emacs-pid)
+			     (user-full-name)
+			     (current-time)
+			     (emacs-uptime)
+			     (garbage-collect)
+			     (random)))
+		27 32)))
+    (if (and (string-match-p "[0-9]" myStr)
+	     (string-match-p "[a-f]" myStr))
+	(insert myStr)
+      (insert-gus-selector))))
