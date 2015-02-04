f0f78de990908b51a43ab81a278c40ed72d69cb4
nickprescott <prescott.nick@gmail.com>
2015-02-04 10:19:28 -0500

    Avoid potential name collisions
    Add an anonymous function wrapper to avoid potential name collisions with any scripts running on the page.

--- links_bookmarklet.js
+++ links_bookmarklet.js
@@ -1,1 +1,1 @@
-javascript:var a=[],i=0,l=document.links;for(i;i<l.length;i++){a.push(l[i])};console.log(a.join('\n'));
+javascript:(function () {var a=[],i=0,l=document.links;for(i;i<l.length;i++){a.push(l[i])};console.log(a.join('\n'));})();
